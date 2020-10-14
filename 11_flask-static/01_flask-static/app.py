c128df094ae03be050a5d92eef3434ebc454e544
michelle-thaung <71651797+michelle-thaung@users.noreply.github.com>
2020-10-14 19:13:04 -0400

    k11

---
+++ 11_flask-static/01_flask-static/app.py
@@ -0,0 +1,23 @@
+# Team MESS (Stella Oh, Ethan Shenker, Michelle Thuang)
+# SoftDev
+# K11 -- Some Things Never Change
+# 2020-10-14
+
+
+# DEMO
+# basics of /static folder
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo queso!"
+
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
