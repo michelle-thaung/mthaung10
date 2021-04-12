c4c2dc9b48148c770b571721575beb83a9016532
michelle-thaung <71651797+michelle-thaung@users.noreply.github.com>
2021-04-12 18:39:29 -0400

    added comments

--- 21_js/intro.js
+++ 21_js/intro.js
@@ -5,7 +5,7 @@ K21 - Get Scripty
 2021-04-12
 */ 
 
-function factI(n) {
+function factI(n) { // return factorial of n iteratively
     let x = 1;
 
     for (w = 2; w <= n; w++) {
@@ -15,18 +15,18 @@ function factI(n) {
     return x;
 }
 
-console.log(factI(5));
+// console.log(factI(5));
 
-function factR(n) {
+function factR(n) { // return factorial of n recursively
     if (n == 0) return 1;
 
     return n * factR(n - 1);
 }
 
-console.log(factR(0));
-console.log(factR(4));
+// console.log(factR(0));
+// console.log(factR(4));
 
-function fibI(n) {
+function fibI(n) { // return nth Fibonacci number iteratively; 0 is the first number
     let [a, b] = [0, 1];
 
     for (i = 1; i < n; i++) {
@@ -36,13 +36,13 @@ function fibI(n) {
     return a;
 }
 
-console.log(fibI(5));
+// console.log(fibI(5));
 
-function fibR(n) {
+function fibR(n) {  // return nth Fibonacci number recursively; 0 is the first number
     if (n == 1) return 0;
     if (n == 2) return 1;
 
     return fibR(n-1) + fibR(n-2);
 }
 
-console.log(fibI(6));
+// console.log(fibI(6));
